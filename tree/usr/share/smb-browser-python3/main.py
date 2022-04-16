b8b3276d16531dc2e83c87145593814cf99778ac
TheTechRobo <52163910+TheTechRobo@users.noreply.github.com>
2022-04-15 22:45:42 -0400

    Still cant figure  it out future proofing

--- tree/usr/share/smb-browser-python3/main.py
+++ tree/usr/share/smb-browser-python3/main.py
@@ -3,14 +3,20 @@ import gi,subprocess
 
 class EmptyClass: pass
 
+def _(string):
+    """
+    Future-proofing.
+    """
+    return string
+
 gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk, Gdk
 
 class DialogExample(Gtk.Dialog):
     def __init__(self, parent):
-        super().__init__(title="Set Mount Format", transient_for=parent, flags=0)
+        super().__init__(title=_("Set Mount Format"), transient_for=parent, flags=0)
         self.add_buttons(
-            "CANCEL", 78111112, "Read Only", 42.0, "Read/Write", 69
+            _("CANCEL"), 78111112, _("Read Only"), 42.0, _("Read/Write"), 69
         )
 
         self.set_default_size(150, 100)
@@ -23,7 +29,7 @@ class DialogExample(Gtk.Dialog):
 class MyWindow(Gtk.Window):
     def __init__(self):
         # MAIN BEHIND THE SCENES INIT
-        super().__init__(title="SMB Browser Revamped")
+        super().__init__(title=_("SMB Browser Revamped"))
 
         dialog = Gtk.MessageDialog(
             transient_for=self,flags=0,
@@ -32,8 +38,8 @@ class MyWindow(Gtk.Window):
             text="Note",
         )
         dialog.format_secondary_text(
-            "This program is only for browsing shares on other computers. If you would like to share files on your computer simply open Thunar, right-click on the directory you want to share and select the option 'Share'. If you want to manage your Shared configurations or set advanced options run 'Samba Admin', you can find it in your menu."
-            "\n\tYour computer will scan for shares when you press OK. SMB-Browser might freeze for a few seconds."
+            _("This program is only for browsing shares on other computers. If you would like to share files on your computer simply open Thunar, right-click on the directory you want to share and select the option 'Share'. If you want to manage your Shared configurations or set advanced options run 'Samba Admin', you can find it in your menu."
+            "\n\tYour computer will scan for shares when you press OK. SMB-Browser might freeze for a few seconds.")
         )
         dialog.run()
 
@@ -53,9 +59,9 @@ class MyWindow(Gtk.Window):
         self.Widgets = EmptyClass()
         # NOW WE'RE ACTUALLY DOING SHIT
         self.Tabs.tab2 = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
-        self.Widgets.setmountform = Gtk.Button(label="Read only or read-write?")
+        self.Widgets.setmountform = Gtk.Button(label=_("Read only or read-write?"))
         self.Widgets.mountparams = Gtk.Entry()
-        self.Tabs.tab2.pack_start(Gtk.Label(label="Params to pass to the mount command"), True, True, 0)
+        self.Tabs.tab2.pack_start(Gtk.Label(label=_("Params to pass to the mount command")), True, True, 0)
         self.Tabs.tab2.pack_start(self.Widgets.mountparams, True, True, 0)
         self.Widgets.setmountform.connect("clicked", self.mntform)
         self.Tabs.tab2.pack_start(self.Widgets.setmountform, True, True, 0)
@@ -71,7 +77,7 @@ class MyWindow(Gtk.Window):
         self.column = Gtk.TreeViewColumn(cell_renderer=self.renderer, text=0, weight=1)
         self.Widgets.ServerListView.append_column(self.column)
         self.Widgets.ServerListView.get_selection().connect("changed", self.showData)
-        self.Widgets.MountBtm = Gtk.Button(label="Mount")
+        self.Widgets.MountBtm = Gtk.Button(label=_("Mount"))
         self.Widgets.MountBtm.connect("clicked", self.mount)
 
         self.Widgets.ShareListBox = Gtk.TreeStore(str)
@@ -83,22 +89,22 @@ class MyWindow(Gtk.Window):
         self.Tabs.tab0.pack_start(self.Widgets.ServerListView, True, True, 5)
         self.Tabs.tab0.pack_start(self.Widgets.ShareListView, True, True, 5)
 
-        self.notebook.append_page(self.Tabs.tab0, Gtk.Label(label="Main"))
-        self.notebook.append_page(self.Tabs.tab2, Gtk.Label(label="Mount options"))
-        self.about = Gtk.Button(label="About")
+        self.notebook.append_page(self.Tabs.tab0, Gtk.Label(label=_("Main")))
+        self.notebook.append_page(self.Tabs.tab2, Gtk.Label(label=_("Mount options")))
+        self.about = Gtk.Button(label=_("About"))
         self.about.connect("clicked", self.about_box)
         self.box.pack_start(self.about, True, True, 10)
-        self.box.pack_start(Gtk.Label(label="SMB-Browser Revamped by TheTechRobo"), True, True, 0)
-        self.quit = Gtk.Button(label="Quit")
+        self.box.pack_start(Gtk.Label(label=_("SMB-Browser Revamped by TheTechRobo")), True, True, 0)
+        self.quit = Gtk.Button(label=_("Quit"))
         self.quit.connect("clicked", self.destroyy)
         self.box.pack_end(self.quit, True, True, 0)
         self.GetServerButon = Gtk.Button(label="DEBUG")
         self.GetServerButon.connect("clicked",self.mount)
-        self.box.pack_end(self.GetServerButon, True, True, 0)
+        #self.box.pack_end(self.GetServerButon, True, True, 0)
         self.getShareData("192.168.2.248")
     def selfol(self, s):
         dialog = Gtk.FileChooserDialog(
-            title="Select a mount point",
+            title=_("Select a mount point"),
             parent=self,
             action=Gtk.FileChooserAction.SELECT_FOLDER,
         )
@@ -114,7 +120,8 @@ class MyWindow(Gtk.Window):
         dialog.destroy()
     def mount(self, s):
         self.selfol(s)
-        command = ["xterm", "-e", f"sudo mount -t cifs //i{self.ipChosen}/{self.share} && python -c 'input(\"Press ENTER to continue\")'"]
+        pressenter = _("Press ENTER to continue")
+        command = ["xterm", "-e", f"sudo mount -t cifs //i{self.ipChosen}/{self.share} && python -c 'input(\"{pressenter}\")'"]
         subprocess.run(command)
     def showData(self, sel):
         model, treeiter = sel.get_selected()
@@ -176,18 +183,18 @@ class MyWindow(Gtk.Window):
         dialog.destroy()
     def about_box(self, widget):
         # { Licence data - feel free to skip
-        licence_data = """
+        licence_data = _("""
         This program was designed to assist users in finding and mounting samba shared folders on a network while being easy yet functional Copyright (C) <2007, 2021>  <David Braker, TheTechRobo>. This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 2 of the License, or (at your option) any later version. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details. You should have received a copy of the GNU General Public License along with this program; if not, write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA. On a Debian GNU/Linux system you can find a copy of this license in `/usr/share/common-licenses/'.
-        """
+        """)
         # }
         dialog = Gtk.MessageDialog(
             transient_for=self,flags=0,
             message_type=Gtk.MessageType.INFO,
             buttons=Gtk.ButtonsType.OK,
-            text="About SMB Browser Revamped",
+            text=_("About SMB Browser Revamped"),
         )
         dialog.format_secondary_text(
-            f"SMB Browser Revamped version 1.0. Created by TheTechRobo with some borrowed code from the original.\nNo, I don't like legalese either, but it keeps the lawyers away.\n{licence_data}" #fuck %s and .format
+            _(f"SMB Browser Revamped version 1.0. Created by TheTechRobo with some borrowed code from the original.\nNo, I don't like legalese either, but it keeps the lawyers away.\n{licence_data}") #fuck %s and .format
         )
         dialog.run()
 
